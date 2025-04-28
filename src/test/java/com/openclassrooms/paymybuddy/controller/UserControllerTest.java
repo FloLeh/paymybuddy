c6baefde2664d5309c0d1a6333d8ce842f1770ae
FloLeh <florianlehuede@gmail.com>
2025-04-28 16:28:10 +0200

    wip test user controller

---
+++ src/test/java/com/openclassrooms/paymybuddy/controller/UserControllerTest.java
@@ -0,0 +1,38 @@
+package com.openclassrooms.paymybuddy.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void testLoginPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testLoginWithWrongCredentials() throws Exception {
+        mockMvc.perform(formLogin("/login").user("user").password("wrong"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void testLoginWithRightCredentials() throws Exception {
+        mockMvc.perform(formLogin("/login").user("test@example").password("admin"))
+                .andExpect(status().is3xxRedirection());
+    }
+}
